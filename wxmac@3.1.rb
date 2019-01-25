ac3d8484a0274f3fe1656436a84dbe24c188624e
MALU <contact@andantissimo.jp>
2019-01-26 01:57:30 +0900

    wxWidgets 3.1.2

---
+++ wxmac@3.1.rb
@@ -0,0 +1,58 @@
+class WxmacAT31 < Formula
+  desc "Cross-platform C++ GUI toolkit (wxWidgets for macOS)"
+  homepage "https://www.wxwidgets.org"
+  url "https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.2/wxWidgets-3.1.2.tar.bz2"
+  sha256 "4cb8d23d70f9261debf7d6cfeca667fc0a7d2b6565adb8f1c484f9b674f1f27a"
+  head "https://github.com/wxWidgets/wxWidgets.git"
+
+  option "with-stl", "use standard C++ classes for everything"
+  option "with-static", "build static libraries"
+
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "libtiff"
+
+  def install
+    args = [
+      "--prefix=#{prefix}",
+      "--enable-clipboard",
+      "--enable-controls",
+      "--enable-dataviewctrl",
+      "--enable-display",
+      "--enable-dnd",
+      "--enable-graphics_ctx",
+      "--enable-std_string",
+      "--enable-svg",
+      "--enable-unicode",
+      "--enable-webkit",
+      "--with-expat",
+      "--with-libjpeg",
+      "--with-libpng",
+      "--with-libtiff",
+      "--with-opengl",
+      "--with-osx_cocoa",
+      "--with-zlib",
+      "--disable-precomp-headers",
+      # This is the default option, but be explicit
+      "--disable-monolithic",
+      # Set with-macosx-version-min to avoid configure defaulting to 10.5
+      "--with-macosx-version-min=#{MacOS.version}",
+    ]
+
+    args << "--enable-stl" if build.with? "stl"
+    args << (build.with?("static") ? "--disable-shared" : "--enable-shared")
+
+    system "./configure", *args
+    system "make", "install"
+
+    # wx-config should reference the public prefix, not wxmac's keg
+    # this ensures that Python software trying to locate wxpython headers
+    # using wx-config can find both wxmac and wxpython headers,
+    # which are linked to the same place
+    inreplace "#{bin}/wx-config", prefix, HOMEBREW_PREFIX
+  end
+
+  test do
+    system bin/"wx-config", "--libs"
+  end
+end
